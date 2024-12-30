5db60205efa6aa3d172c773d64537d11cb3b2eb8
EslamKamel89 <elsamkamelforex@gmail.com>
2024-12-30 12:40:43 +0200

    adding the main styles , html markup and removing unnecessary files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
+<template>
+  <!-- Form for Adding Photos -->
+  <form>
+    <input placeholder="Enter image URL" />
+    <button type="submit">Add Photo</button>
+  </form>
+
+  <!-- Conditional Rendering -->
+  <p class="no-photos">No photos added yet. Add some!</p>
+
+  <div class="gallery">
+    <!-- List Rendering and Conditional Class Binding -->
+    <div class="photo">
+      <!-- An image goes here -->
+      <button class="favorite-button">★</button>
+    </div>
+  </div>
+</template>
+
 <script lang="ts">
 export default {
-  setup() {},
+  setup() {
+    // Keep track of the list of photos
+    // We need a ref for the currently entered URL
+    const addPhoto = () => {
+      // Adding photo logic goes here
+    }
+    const toggleFavorite = () => {
+      // Favorite toggling logic
+    }
+
+    return {
+      addPhoto,
+      toggleFavorite,
+    }
+  },
 }
 </script>
-
-<template>
-  <h1>Hello world</h1>
-</template>
 
 <style></style>
