14a738107b26eb17f3c6d7fb023704077e8a9bec
EslamKamel89 <elsamkamelforex@gmail.com>
2024-12-30 13:16:49 +0200

    rendering the entered image

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,45 @@
 <template>
   <!-- Form for Adding Photos -->
-  <form>
-    <input placeholder="Enter image URL" />
+  <form @submit.prevent="addPhoto">
+    <input placeholder="Enter image URL" v-model="newPhotoUrl" />
     <button type="submit">Add Photo</button>
   </form>
 
   <!-- Conditional Rendering -->
-  <p class="no-photos">No photos added yet. Add some!</p>
+  <p v-if="photos?.length === 0" class="no-photos">No photos added yet. Add some!</p>
 
-  <div class="gallery">
+  <div v-else class="gallery">
     <!-- List Rendering and Conditional Class Binding -->
-    <div class="photo">
-      <!-- An image goes here -->
-      <button class="favorite-button">★</button>
+    <div v-for="photo in photos" :key="photo">
+      <div class="photo">
+        <!-- An image goes here -->
+        <img :src="photo" alt="photo not found" />
+        <button @click="toggleFavorite" class="favorite-button">★</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
+
 export default {
   setup() {
     // Keep track of the list of photos
+    const photos = ref<string[]>([])
     // We need a ref for the currently entered URL
+    const newPhotoUrl = ref<string>('')
     const addPhoto = () => {
-      // Adding photo logic goes here
+      photos.value.push(newPhotoUrl.value)
+      newPhotoUrl.value = ''
     }
-    const toggleFavorite = () => {
-      // Favorite toggling logic
-    }
+    const toggleFavorite = () => {}
 
     return {
       addPhoto,
       toggleFavorite,
+      photos,
+      newPhotoUrl,
     }
   },
 }
