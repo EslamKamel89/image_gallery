cead888c43c75359cab9a310982c1e93a0c9622b
EslamKamel89 <elsamkamelforex@gmail.com>
2024-12-30 14:42:12 +0200

    adding favorite and delete photo function

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,17 @@
 
   <div v-else class="gallery">
     <!-- List Rendering and Conditional Class Binding -->
-    <div v-for="photo in photos" :key="photo">
-      <div class="photo">
+    <div v-for="photo in photos" :key="photo.id">
+      <div class="photo" @click="deletePhoto(photo.id)">
         <!-- An image goes here -->
-        <img :src="photo" alt="photo not found" />
-        <button @click="toggleFavorite" class="favorite-button">★</button>
+        <img :src="photo.img" alt="photo not found" />
+        <button
+          @click.stop="toggleFavorite(photo.id)"
+          class="favorite-button"
+          :class="{ active: photo.isFavorite }"
+        >
+          ★
+        </button>
       </div>
     </div>
   </div>
@@ -22,24 +28,38 @@
 
 <script lang="ts">
 import { ref } from 'vue'
-
+import pr from './utils/pr'
+type Photo = {
+  id: number
+  img: string
+  isFavorite: boolean
+}
 export default {
   setup() {
     // Keep track of the list of photos
-    const photos = ref<string[]>([])
+    const photos = ref<Photo[]>([])
     // We need a ref for the currently entered URL
     const newPhotoUrl = ref<string>('')
     const addPhoto = () => {
-      photos.value.push(newPhotoUrl.value)
+      if (newPhotoUrl.value == '') return
+      photos.value.push({ id: Math.random(), img: newPhotoUrl.value, isFavorite: false })
       newPhotoUrl.value = ''
     }
-    const toggleFavorite = () => {}
-
+    const toggleFavorite = (id: number) => {
+      const index = photos.value.findIndex((photo) => photo.id === id)
+      photos.value[index].isFavorite = !photos.value[index].isFavorite
+    }
+    const deletePhoto = (id: number) => {
+      pr('deletePhoto')
+      photos.value = photos.value.filter((photo) => photo.id !== id)
+    }
     return {
       addPhoto,
       toggleFavorite,
       photos,
       newPhotoUrl,
+      pr,
+      deletePhoto,
     }
   },
 }
---
+++ src/utils/pr.ts
@@ -0,0 +1,4 @@
+export default function pr<T>(value: T): T {
+  console.log(value)
+  return value
+}
